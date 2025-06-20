e899bfeba4824a597218c41971e9245463fa88d0
Keeva <keeva.connolly@qcif.edu.au>
2025-06-20 15:50:13 +1000

    included a second section to write a supplementary markdown file to add to the genome note markdown including sample and experiment metadata for hi-c data

--- src/atol_genome_note_lite/explore.py
+++ src/atol_genome_note_lite/explore.py
@@ -1,12 +1,15 @@
 import json
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader, Undefined
 
 print("Starting script")
 
 # path_to_template = args.template
-path_to_template = "dev/not-a-genome-note_template.md"
-path_to_output = "results/genome_note.md"
+path_to_template = "dev/not-a-genome-note_template_scaffold-level.md"
+path_to_genome_note_output = "results/genome_note_scaffold-level.md"
 path_to_sample_metadata = "dev/sample_1.json"
+path_to_supplement_template = "dev/hic_data_template.md"
+path_to_supplement_output = "results/hic_data_for_genome_note.md"
+path_to_supplement_metadata = "dev/sample_2.json"
 
 # read the template
 with open(path_to_template, "rt", encoding="utf-8") as f:
@@ -18,6 +21,22 @@ with open(path_to_sample_metadata, "rt") as f:
     sample_metadata = json.load(f)
 
 # render the template
-print(f"Writing output to {path_to_output}")
-with open(path_to_output, "wt", encoding="utf-8") as f:
+print(f"Writing output to {path_to_genome_note_output}")
+with open(path_to_genome_note_output, "wt", encoding="utf-8") as f:
     f.write(template.render(sample_metadata))
+
+#repeat the above for suplementary metadata (for Hi-C data)
+
+# read the template
+with open(path_to_supplement_template, "rt", encoding="utf-8") as f:
+    sup_template = Template(f.read())
+
+# read the sample metadata
+print(f"Reading supplementary sample metadata from {path_to_supplement_metadata}")
+with open(path_to_supplement_metadata, "rt") as f:
+    supplement_metadata = json.load(f)
+
+# render the template
+print(f"Writing supplementary output to {path_to_supplement_output}")
+with open(path_to_supplement_output, "wt", encoding="utf-8") as f:
+    f.write(sup_template.render(supplement_metadata))
