10a49021a419d88b4972628ea85cb2d22c3767e5
Keeva <keeva.connolly@qcif.edu.au>
2026-01-06 09:06:01 +1000

    enabled 0-n input arguments for Hi-C metadata files, processed metadata are now saved to intermediate files with the suffix '-processed' (original files are no longer overwritten), processed metadata files and supplementary rendered templates are now deleted after inclusion into the main template

--- src/atol_genome_note_lite/explore.py
+++ src/atol_genome_note_lite/explore.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import json
 import argparse
-from pathlib import Path
+import json
+import os
 from jinja2 import Template, Environment, FileSystemLoader, Undefined
+from pathlib import Path
 
 print("Starting script")
 
@@ -13,6 +14,7 @@ path_to_extract_supplement_output = "templates/supplementary_extract_data_for_ge
 path_to_seq_supplement_output = "templates/supplementary_seq_data_for_genome_note.md"
 path_to_bpa_package_supplement_output = "templates/supplementary_package_data_for_genome_note.md"
 
+# set input arguments
 argument_parser = argparse.ArgumentParser(
     description="This tool generates a draft genome note lite markdown document based on sample, read, assembly, and annotation metadata."
 )
@@ -22,13 +24,13 @@ input_group.add_argument(
     "--wgs_metadata",
     nargs="+",
     type=Path,
-    help="at least one JSON file listing metadata for a PacBio WGS sample and sequencing run. The first file should also contain assembly metadata and optionally annotation metadata."
+    help="at least one JSON file listing metadata for a WGS sample and sequencing run. The first file should also contain assembly metadata and optionally annotation metadata."
 )
 input_group.add_argument(
     "--hic_metadata",
-    nargs="?",
+    nargs="*",
     type=Path,
-    help="an optional JSON file listing metadata for a Hi-C sample and sequencing run."
+    help="optional JSON file/s listing metadata for a Hi-C sample and sequencing run."
 )
 output_group.add_argument(
     "--output",
@@ -48,7 +50,7 @@ argument_parser.add_argument(
 )
 args = argument_parser.parse_args()
 
-#TODO: check that arguments are valid paths
+# TODO: check that arguments are valid paths
 
 # updating metadata input to remove metadata containing empty strings
 def overwrite_empty_strings(metadata):
@@ -93,27 +95,33 @@ def map_bpa_initiative(metadata):
                 key_value['bpa_initiative'] = None
     return metadata
 
+processed_wgs_file_paths = []
+processed_hic_file_paths = []
+
 # preprocessing metadata for input WGS metadata
 for input_file in args.wgs_metadata:
     with open(input_file, 'r') as f:
         unprocessed_sample_metadata = json.load(f)
-
         sample_metadata_w_initiative = map_bpa_initiative(unprocessed_sample_metadata)
         processed_sample_metadata = overwrite_empty_strings(sample_metadata_w_initiative)
+    processed_file = Path(input_file).stem + "-processed" + Path(input_file).suffix
 
-    with open(input_file, 'w') as f:
+    with open((processed_file), 'w') as f:
         json.dump(processed_sample_metadata, f)
+    processed_wgs_file_paths.append(processed_file)
 
 # preprocessing metadata for input Hi-C metadata if provided
 if args.hic_metadata is not None:
-    with open(args.hic_metadata, 'r') as f:
-        unprocessed_hic_sup_metadata = json.load(f)
+    for input_file in args.hic_metadata:
+        with open(input_file, 'r') as f:
+            unprocessed_hic_sup_metadata = json.load(f)
+            hic_sup_metadata_w_initiative = map_bpa_initiative(unprocessed_hic_sup_metadata)
+            processed_hic_sup_metadata = overwrite_empty_strings(hic_sup_metadata_w_initiative)
+        processed_file = Path(input_file).stem + "-processed" + Path(input_file).suffix
 
-    hic_sup_metadata_w_initiative = map_bpa_initiative(unprocessed_hic_sup_metadata)
-    processed_hic_sup_metadata = overwrite_empty_strings(hic_sup_metadata_w_initiative)
-
-    with open(args.hic_metadata, 'w') as f:
-        json.dump(processed_hic_sup_metadata, f)
+        with open((processed_file), 'w') as f:
+            json.dump(processed_hic_sup_metadata, f)
+        processed_hic_file_paths.append(processed_file)
 
 # functions to make things in the template pretty
 def make_pretty_number(ugly_number):
@@ -157,12 +165,13 @@ class undefined_tokens(Undefined):
 env = Environment(loader=FileSystemLoader("templates"),undefined=undefined_tokens)
 
 # initialise lists for cross-checking sample and library ids (to avoid duplicating metadata in the genome note lite)
-all_input_files = list(args.wgs_metadata)
-input_sample_ids = []
-input_library_ids = []
+all_input_files = list(processed_wgs_file_paths)
 
 if args.hic_metadata is not None:
-    all_input_files.append(args.hic_metadata)
+    all_input_files.extend(processed_hic_file_paths)
+
+input_sample_ids = []
+input_library_ids = []
 
 # read Hi-C and supplementary WGS input metadata and render supplementary helper files
 for idx, file in enumerate(all_input_files):
@@ -231,9 +240,10 @@ for idx, file in enumerate(all_input_files):
             combined_package_sup = package_supplement_md + sup_package_render
             with open(path_to_bpa_package_supplement_output, "wt", encoding="utf-8") as f:
                 f.write(combined_package_sup)
-        # append sample and library ids to catalogue list
-        input_sample_ids.append(sample_name)
-        input_library_ids.append(library_name)
+        else:
+            next(enumerate(all_input_files))
+    input_sample_ids.append(sample_name)
+    input_library_ids.append(library_name)
 
 # initialise main template
 if args.w_annotation:
@@ -247,22 +257,20 @@ else:
     template = env.get_template("not-a-genome-note-lite-template.md")
 
 # read the sample metadata
-print(f"Reading sample metadata from {args.wgs_metadata[0]}")
-with open(args.wgs_metadata[0], "rt") as f:
-    sample_metadata = json.load(f)
+print(f"Reading metadata from {processed_wgs_file_paths[0]}")
+with open(processed_wgs_file_paths[0], "rt") as f:
+    assembly_sample_metadata = json.load(f)
 
 # render the core template, integrating the supplementary markdown files for hi-c and/or secondary wgs data
 print(f"Combining and writing output to {args.output}")
 with open(args.output, "wt", encoding="utf-8") as f:
-    f.write(template.render(sample_metadata,make_pretty_number=make_pretty_number,round_bases_up=round_bases_up,round_decimal=round_decimal,add_spaces=add_spaces))
+    f.write(template.render(assembly_sample_metadata,make_pretty_number=make_pretty_number,round_bases_up=round_bases_up,round_decimal=round_decimal,add_spaces=add_spaces))
 
-#wipe supplementary markdown files
-print("Wiping supplementary helper files and ending script")
-with open(path_to_sample_supplement_output, "wt") as f:
-    f.close()
-with open(path_to_extract_supplement_output, "wt") as f:
-    f.close()
-with open(path_to_seq_supplement_output, "wt") as f:
-    f.close()
-with open(path_to_bpa_package_supplement_output, "wt") as f:
-    f.close()
+# remove processed metadata and supplementary markdown files
+print("Removing processed metadata and supplementary helper files and ending script")
+for file in all_input_files:
+    os.remove(file)
+
+for file in [path_to_sample_supplement_output, path_to_extract_supplement_output, path_to_seq_supplement_output, path_to_bpa_package_supplement_output]:
+    if os.path.exists(file):
+        os.remove(file)
