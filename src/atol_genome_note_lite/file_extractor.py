b8e5ca4ced4e94a496decbafe92fe3bc18492d47
Keeva <keeva.connolly@qcif.edu.au>
2026-02-23 15:57:38 +1000

    adding command line arguments for file_extractor.py

--- src/atol_genome_note_lite/file_extractor.py
+++ src/atol_genome_note_lite/file_extractor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import logging
 import re
-# from pathlib import Path
+from pathlib import Path
 
 # configure logger
 logger = logging.getLogger(__name__)
@@ -17,12 +18,33 @@ file_handler.setFormatter(formatter)
 logger.setLevel("DEBUG")
 console_handler.setLevel("INFO")
 
+# add arguments
+argument_parser = argparse.ArgumentParser(
+    description="This tool extracts file paths for files used in the genome note lite from a text file containing the directory contents generated in the genome assembly pipeline.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+argument_parser.add_argument(
+    "--file_dir",
+    type=Path,
+    help="A text file listing all file paths contained in the genome assembly pipeline ouptut directory."
+)
+argument_parser.add_argument(
+    "--tolid",
+    help="The ToLID for the specimen used to generate the genome assembly."
+)
+argument_parser.add_argument(
+    "--output",
+    type=Path,
+    default=Path("results/found_files.json"),
+    help="A JSON file containing the list of extracted paths with their file types."
+)
+args = argument_parser.parse_args()
+
+# TODO: handle output directories and file paths for assemblies generated without hi-c
+
 # set global variables
-file_dir = # file directory path
-tolid = # tolid
-# hic = True
+tolid = args.tolid
 file_paths = {}
-output_json = "results/found_files.json"
 
 patterns = {
     'busco_stats': f"{tolid}\\.hifiasm-hic.*/scaffolding_hap1/yahs/asm_hap1_scaffolds_final.*short_summary.json",
@@ -49,13 +71,13 @@ def find_file(file_type, pattern):
 #        path_list[0] = Path(path_list[0]) # convert string to Path
     return path_list
 
-with open(file_dir, "r") as f:
+with open(args.file_dir, "r") as f:
     file_names = f.read()
 
 for file_type, pattern in patterns.items():
     path = find_file(file_type, pattern)
     file_paths[file_type] = path
 
-with open(output_json, "wt", encoding="utf-8") as f:
-    logger.info(f"Writing output to {output_json}")
+with open(args.output, "wt", encoding="utf-8") as f:
+    logger.info(f"Writing file paths for {args.tolid} to {args.output}")
     json.dump(file_paths, f)
