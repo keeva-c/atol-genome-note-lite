c0c7f8509b638b59bb007defee82830f0b0be6f4
Keeva <keeva.connolly@qcif.edu.au>
2026-06-18 08:17:41 +1000

    patch for grasslands

--- src/atol_genome_note_lite/explore.py
+++ src/atol_genome_note_lite/explore.py
@@ -132,6 +132,8 @@ def map_bpa_initiative(metadata):
         full_initiative = 'Genomics for Forest Resilience Initiative'
     elif initiative_acronym == 'bpa-plants':
         full_initiative = 'Genomics for Australian Plants Initiative'
+    elif initiative_acronym == 'grasslands':
+        full_initiative = 'Australian Grasslands Initiative'
     elif initiative_acronym == 'aus-venom':
         full_initiative = 'Australian Venom Innovation and Discovery Initiative'
     else:
